a7d8c4f0c75fc033956012f1767f83f86dcc06b2
Dang Van Hung <Hung.DangVan@nttdata.com>
2018-03-29 16:25:03 +0700

    add home component

--- APM-Final/src/app/app.module.ts
+++ APM-Final/src/app/app.module.ts
@@ -19,8 +19,7 @@ import { ProductModule } from './products/product.module';
         { path: 'welcome', component: WelcomeComponent },
         { path: '', redirectTo: 'welcome', pathMatch: 'full'},
         { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ]),
-    ProductModule
+    ])
   ],
   bootstrap: [AppComponent]
 })

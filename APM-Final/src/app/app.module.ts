59289894ddbd1fbbc90c195a541ca0c9f6246870
Dang Van Hung <Hung.DangVan@nttdata.com>
2018-03-29 17:42:09 +0700

    update home component

--- APM-Final/src/app/app.module.ts
+++ APM-Final/src/app/app.module.ts
@@ -6,20 +6,24 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
+import { HomeComponent } from './home/home.compoment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-        { path: 'welcome', component: WelcomeComponent },
-        { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+        { path: 'home', component: HomeComponent },
+        { path: '', redirectTo: 'home', pathMatch: 'full'},
+        { path: 'welcome', component: WelcomeComponent , pathMatch: 'full'},
         { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ APM-Final/src/app/home/home.compoment.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+@Component({
+    templateUrl: './home.component.html'
+})
+export class HomeComponent{
+    public pageTitle: string ='Home tilte';
+}
